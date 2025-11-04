c45217f628f3a274b174b6c0f871dfd5616f0f76
eunseo <2023180012@tukorea.ac.kr>
2025-11-04 21:42:42 +0900

    일단 프레임 증가량으로 프레임 수정 부분 수정

--- bird.py
+++ bird.py
@@ -8,6 +8,10 @@ RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)    # meter / minute
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)    # meter / second
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)   # pixel / second
 
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 5
+
 class Bird:
     def __init__(self, x=400, y=300):
         self.image = load_image('bird_animation.png')
@@ -16,11 +20,11 @@ class Bird:
         self.dir = 1   # 1: right, -1: left
 
     def update(self):
-        self.frame = (self.frame + 1) % 5
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 5
         self.x += self.dir * RUN_SPEED_PPS * game_framework.frame_time
 
     def draw(self):
-        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+        self.image.clip_draw((int)(self.frame * 100), 0, 100, 100, self.x, self.y)
 
     def handle_events(self):
         pass
