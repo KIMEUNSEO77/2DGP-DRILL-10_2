49677075e7a77fc1913a4ed87f7867dd654b053a
eunseo <2023180012@tukorea.ac.kr>
2025-11-04 21:22:08 +0900

    Bird클래스 생성

---
+++ bird.py
@@ -0,0 +1,13 @@
+from pico2d import load_image, get_time
+
+class Bird:
+    image = None
+
+    def __init__(self):
+        pass
+    def update(self):
+        pass
+    def draw(self):
+        pass
+    def handle_events(self):
+        pass
